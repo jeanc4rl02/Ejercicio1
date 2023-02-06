187c42c2053bd8e85fa9b1c0ae25cc7bad8e1913
jeanc4rl02 <jean.carrillo@udea.edu.co>
2023-02-06 18:07:22 -0500

    lógica V1

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ var app = new Vue({
         year: 1985,
       },
     ],
+    is: {
+      new: false,
+      play: false,
+    },
     anioRandom: 0,
     anioIngresado: 1985,
     contador: 0,
@@ -30,31 +34,62 @@ var app = new Vue({
     mensajeMenor: false,
     ganador: false,
     pista: false,
+    perdio: false,
 
   },
   methods: {
-    jugar() {
+    generarAño(){
       let random = Math.floor(Math.random()*(this.usuarios.length));
       this.anioRandom = this.usuarios[random].year
-      if(this.anioIngresado > this.anioRandom){
-        this.mensajeMenor = false;
-        this.mensajeMayor = true;
-        this.ganador = false;
-      } else if(this.anioIngresado < this.anioRandom){
-        this.mensajeMenor = true;
-        this.mensajeMayor = false;
-        this.ganador = false;
+      localStorage.setItem("anioRandom", this.anioRandom);
+    },
+    jugar() {
+      if(this.contador < 7){
+          if(this.anioIngresado > this.anioRandom){
+          this.mensajeMenor = false;
+          this.mensajeMayor = true;
+          this.ganador = false;
+          this.contador += 1
+          } else if(this.anioIngresado < this.anioRandom){
+            this.mensajeMenor = true;
+            this.mensajeMayor = false;
+            this.ganador = false;
+            this.contador += 1
+          } else {
+            this.mensajeMenor = false;
+            this.mensajeMayor = false;
+            this.ganador = true;
+            this.contador += 1
+          }
+
+          if(this.contador >= 2){
+            this.pista = true;
+          } else {
+            this.pista = false;
+          }
       } else {
-        this.mensajeMenor = false;
-        this.mensajeMayor = false;
-        this.ganador = true;
+        this.perdio = true;
       }
-      this.contador += 1
-
-      if(this.contador >= 2){
-        this.pista = true;
-      }
-
+      
+      console.log("año "+this.anioRandom);
+      console.log("es mayor "+this.mensajeMayor);
+      console.log("es menor "+this.mensajeMenor);
+      console.log("ganaste "+this.ganador);
+      console.log("pista "+this.pista);
+      console.log(localStorage.getItem("anioRandom"));
+      console.log("perdió? "+this.perdio);
     }
+  },
+  showCreatedPlayer() {
+    this.is = {
+      new: true,
+      play: false,
+    };
+  },
+  showPlay() {
+    this.is = {
+      new: false,
+      play: true,
+    };
   }
 })
